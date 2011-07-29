4d277daf1634fd042812b80b1dddb6f9a5fd959e
Adam Solove <asolove@gmail.com>
2011-07-29 12:57:04 -0400

    Template render working vaguely.

--- lessdoc.js
+++ lessdoc.js
@@ -1,14 +1,47 @@
+function Example(el){
+	var $el 	  = this.el = $(el),
+		$template = this.el.find(".ld-example"),
+		$options  = this.el.find("dl");
+	
+	if($template.length == 0) return false;
+	
+	this.template = Handlebars.compile($template.find("code").html());
+	$template.remove();
+	
+	this.options = [];
+	// loop through $options
+	
+	this.data = {
+		icon:    "new_page",
+		style:   "btn1",
+		content: "text",
+		state:   "active",
+		color:   "green"
+	};
+	$options.remove();
+	
+	console.log($el);
+	this.elements = {
+		el:   	 $el,
+		code: 	 $("<pre>").appendTo($el),
+		preview: $("<div>").appendTo($el)
+	};
+	
+	this.render();
+	console.log("Rendered template")
+	return this;
+};
+
+Example.prototype.render = function(){
+	var view = this.template(this.data);
+	
+	this.elements.code.text(view);
+	this.elements.preview.html(view);
+};
+
 jQuery(function(){
-	$(".ld-example code").html(function(template){
-		var $this = $(this),
-				$comm = $this.prev(".ld-comment"),
-				data  = {};
-				
-		$comm.find("dt").each(function(){
-			data[$(this).text()] = $(this).next("dd").text().split(", ")[0];
-		});
-		
-		console.log(data);
-		
+	var examples = [];
+	$(".ld-comment").each(function(){
+		examples.push(new Example(this));
 	});
 });
